f43592f7d0e2152dde00faa21fca8af4e6bf3fb4
Edwin Rivera <edwriver@bancolombia.com.co>
2025-02-17 11:53:21 -0500

    Agregando el interceptor para el manejo de las peticiones http

---
+++ src/main/java/co/com/coban/config/WebConfig.java
@@ -0,0 +1,19 @@
+package co.com.coban.config;
+
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    private AuditoriaInterceptor auditoriaInterceptor;
+
+    public WebConfig(AuditoriaInterceptor auditoriaInterceptor) {
+        this.auditoriaInterceptor = auditoriaInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(auditoriaInterceptor);
+    }
+
+}
