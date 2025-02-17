5b37de219548b30347fd698a6319f06b53b5c1d2
Edwin Rivera <edwriver@bancolombia.com.co>
2025-02-17 07:57:17 -0500

    Agregando el mapper de la entidad Cliente

--- src/main/java/co/com/coban/controller/ClienteCrontroller.java
+++ src/main/java/co/com/coban/controller/ClienteCrontroller.java
@@ -1,13 +1,21 @@
 package co.com.coban.controller;
 
+import co.com.coban.dto.ClienteDTO;
 import co.com.coban.entity.Cliente;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import co.com.coban.repository.ClienteRepository;
+import co.com.coban.util.ClienteMapper;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("v1/cliente")
 public class ClienteCrontroller {
+
+    private final ClienteRepository clienteRepository;
+
+    public ClienteCrontroller(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
 
 //    private Cliente cliente;
 //
@@ -19,5 +27,25 @@ public class ClienteCrontroller {
     public String saludo() {
         return "Hola mundo";
     }
+
+
+    // mostrar un cliente quemado para pruebas
+    @GetMapping("/id")
+    public ClienteDTO getCliente() {
+        return new ClienteDTO("Juan", "juan@mail.com", "1234567890", "Calle 123");
+    }
+
+    @PostMapping("/crear")
+    public ClienteDTO crearCliente(@Valid @RequestBody ClienteDTO clienteDTO) {
+        Cliente cliente = ClienteMapper.toCliente(clienteDTO);
+        cliente = clienteRepository.save(cliente);
+        return ClienteMapper.toClienteDTO(cliente);
+    }
+
+    // mostrar todos los clientes
+//    @GetMapping("/all")
+//    public ClienteDTO[] getAllClientes() {
+//
+//    }
 
 }
---
+++ src/main/java/co/com/coban/util/ClienteMapper.java
@@ -0,0 +1,26 @@
+package co.com.coban.util;
+
+import co.com.coban.dto.ClienteDTO;
+import co.com.coban.entity.Cliente;
+
+public class ClienteMapper {
+    public static ClienteDTO toClienteDTO(Cliente cliente) {
+        ClienteDTO clienteDTO = new ClienteDTO();
+        clienteDTO.setId(cliente.getId());
+        clienteDTO.setNombre(cliente.getNombre());
+        clienteDTO.setEmail(cliente.getEmail());
+        clienteDTO.setTelefono(cliente.getTelefono());
+        clienteDTO.setDireccion(cliente.getDireccion());
+        return clienteDTO;
+    }
+
+    public static Cliente toCliente(ClienteDTO clienteDTO) {
+        Cliente cliente = new Cliente();
+        cliente.setId(clienteDTO.getId());
+        cliente.setNombre(clienteDTO.getNombre());
+        cliente.setEmail(clienteDTO.getEmail());
+        cliente.setTelefono(clienteDTO.getTelefono());
+        cliente.setDireccion(clienteDTO.getDireccion());
+        return cliente;
+    }
+}
